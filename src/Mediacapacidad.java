7031b83e791e8ef9e2a81c5929ef5321a3f82f8f
Juan <juananguillo@gmail.com>
2020-01-14 20:27:11 +0100

    Test media capacidad

---
+++ src/Mediacapacidad.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Mediacapacidad {
+
+	DepositoCombustible tank = new DepositoCombustible(20.0,0.0);
+
+	@Test
+	public void valordeposito() {
+		assertEquals(false, tank.estaLleno());
+	
+	}
+
+}
